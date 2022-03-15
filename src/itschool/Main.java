3ad61c0ef74dfeb328a71e7aa3e8945a7d8fb44e
Benziks <joker.nytik@gmail.com>
2022-03-15 17:25:37 +0200

    Initial commit

---
+++ src/itschool/Main.java
@@ -0,0 +1,76 @@
+package itschool;
+
+public class Main
+{
+
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static void main(String[] args) {
+
+        System.out.println(ANSI_BLACK_BACKGROUND  +    "                                                           " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    "                                                           " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    "                                                           " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                   ааааа                   "  + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                  ааааааа                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                 ааааааааа                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +   YELLOW_BOLD + "                                 ааааааааа                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                 ааааааааа                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                аааааааааа                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +    YELLOW_BOLD + "                                аааааааааа                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  + YELLOW_BOLD + "                                аааааааааа                 " + ANSI_RESET);
+        System.out.print(ANSI_BLACK_BACKGROUND  +  "                                фф"+ ANSI_GREEN + "к"+ ANSI_WHITE + "фффф"+ ANSI_GREEN + "к" + ANSI_RESET );
+        System.out.println(ANSI_BLACK_BACKGROUND  + YELLOW_BOLD + "а                  " + ANSI_RESET);
+        System.out.print(ANSI_BLACK_BACKGROUND  + ANSI_WHITE +"                                ф");
+        System.out.print(ANSI_BLACK_BACKGROUND  + YELLOW_BOLD + "аaa" + ANSI_RESET);
+        System.out.print(ANSI_BLACK_BACKGROUND  + ANSI_WHITE + "ффф");
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "аa                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "                                 аааааааа                  " + ANSI_RESET);
+        System.out.print(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                 ааааааа" + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "а                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                ааааааааа                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                ааааааааа                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                ааааааааа                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                 аааааа"+ ANSI_BLACK + "к" + ANSI_YELLOW + "а                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                  аааааа"+ YELLOW_BOLD + "а                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_YELLOW + "                                   аааа"+ YELLOW_BOLD + "аа                  " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "                                   ааа"+ ANSI_WHITE +"фффф                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "                                  ааа"+ ANSI_WHITE +"ффффф                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +YELLOW_BOLD + "                                  аа"+ ANSI_WHITE +"фффффф                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                 ффффффффф                 "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                 фффффффф                  "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                ффффффффф                  "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                фффффффф                   "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                фффффффф                   "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                                ффффффффф                  "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                               ффффффффффф"+ YELLOW_BOLD +"ааа              "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                               фффффффффффф"+ YELLOW_BOLD +"ааааа           "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                              ффффффффффффф"+ YELLOW_BOLD +"ааааааа         "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE + "                              ффффффффффффф"+ YELLOW_BOLD +"ааааааа         "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                             фффффффффффффф"+ YELLOW_BOLD +"ааааааа         "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                             фффффффффффффф"+ YELLOW_BOLD +"ааааааа         "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                             фффффффффффффф"+ YELLOW_BOLD +"аааааааааа      "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE +"                             фффффффффффффф"+ YELLOW_BOLD +"ааааааа         "+ ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +ANSI_WHITE + "                               ффффффффффф                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                               ммммммммммм                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                               ммммммммммм                 " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                                ммммммммммм                " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                                 мммммммммм                " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                                   ммммммммм               " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                                  ммммммммммм              " + ANSI_RESET);
+        System.out.println(ANSI_BLACK_BACKGROUND  +BLUE_BOLD + "                                  ммммммммммм              " + ANSI_RESET);
+
+
+
+
+
+
+
+
+    }
+}
